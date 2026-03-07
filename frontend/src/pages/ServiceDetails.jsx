53a872205aa3988d0d83b29c7660724915a438ab
Amit yadaav <amityadaav707@gmail.com>
2026-03-07 13:36:29 +0530

    updated

--- frontend/src/pages/ServiceDetails.jsx
+++ frontend/src/pages/ServiceDetails.jsx
@@ -240,7 +240,7 @@ import { useState, useEffect } from "react";
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import "./ServiceDetails.css";
+import "./serviceDetails.css";
 
 export default function ServiceDetails() {
   const { service } = useParams();
